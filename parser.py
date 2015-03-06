de73600ae9f65e0b21da9bbfacf8813824084e1a
ArchStacker <ArchStacker+github@gmail.com>
2015-03-06 19:33:42 +0800

    Add the description for some tools

--- parser.py
+++ parser.py
@@ -5,18 +5,18 @@ nodes=[]
 links=[]
 dirnums={}
 def dfs( currnode,parentnum ):
-    nodes.append( {'name':currnode['name'],'icon':currnode['icon'],'power':currnode['power']} )
+    nodes.append( {'name':currnode['name'],'icon':currnode['icon'],'power':currnode['power'],'description':''} )
     currnum = len(nodes)-1
     dirnums[currnode['dirname']] = currnum
     if parentnum is not None:
         links.append( {'source':parentnum,'target':currnum} )
     for num in currnode:
-        if num != 'name' and num != 'dirname' and num != 'icon' and num != 'power' :
+        if num != 'name' and num != 'dirname' and num != 'icon' and num != 'power' and num != 'description' :
             dfs( currnode[num],currnum )
 
 
 config = configparser.ConfigParser( strict=False )
-dirtree = { 'name':'Kali','dirname':'Kali','icon':'kali-menu.png','power':10 }
+dirtree = { 'name':'Kali','dirname':'Kali','icon':'kali-menu.png','power':10,'description':'' }
 destdir = 'Kali/desktop-directories'
 for filename in os.listdir(destdir):
     tdic = dirtree
@@ -35,12 +35,13 @@ for filename in os.listdir(destdir):
         tdic['dirname'] = filename.split('.')[0]
         tdic['icon'] = config['Desktop Entry']['Icon']
         tdic['power'] = power
+        tdic['description'] = ''
 
 dfs( dirtree,None )
 destdir = 'Kali/applications'
 for filename in os.listdir(destdir):
     config.read( os.path.join(destdir,filename) )
-    nodes.append( {'name':config['Desktop Entry']['name'],'icon':'kali-menu.png','power':1} )
+    nodes.append( {'name':config['Desktop Entry']['name'],'icon':'kali-menu.png','power':1,'description':''} )
     currnum = len(nodes)-1
     for category in config['Desktop Entry']['Categories'].split(';'):
         try:
